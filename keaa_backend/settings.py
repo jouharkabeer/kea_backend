d37718d4049272df47be77e05cf3fe4f587498b9
Jouhar Kabeer <jouharkabeer412@gmail.com>
2025-02-17 12:46:25 +0530

    fdgafgdgf

--- keaa_backend/settings.py
+++ keaa_backend/settings.py
@@ -32,7 +32,7 @@ SIMPLE_JWT = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 import os
@@ -42,7 +42,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", SECRET_KEY)
 
 # DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["keaa_backend.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["keaa_backend.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 
